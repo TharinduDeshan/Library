f38a7153f60847e500f099b95659d2ebcb16a229
Vithusha T <vithutheva98@gmail.com>
2022-04-17 00:47:12 +0530

    commit 17

---
+++ BACKEND/modules/Customer.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const CustomerSchema = new Schema({
+    CustomerID : {
+        type : String,
+        required : true
+    },
+
+    Name : {
+        type : String,
+        required : true
+    },
+
+    NIC : {
+        type : String,
+        required : true
+    },
+
+    PhoneNumber : {
+        type : String,
+        required : true,
+    },
+
+    Address : {
+        type : String,
+        required : true
+    },
+
+    Email : {
+            type : String,
+            required : true,
+    },
+
+    Occupcation : {
+        type : String,
+        required : true,
+    },
+
+    Gender : {
+        type : String,
+        required :true,
+        default : "Others"
+    },
+
+    Member : {
+        type : Boolean,
+        required : true,
+        default : true
+    },
+
+})
+
+const Customer = mongoose.model("Customer",CustomerSchema);
+module.exports = Customer;
