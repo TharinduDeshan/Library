e2ea8c3f49654bdf180b80c8bce91bc801f2ab98
Vithusha T <vithutheva98@gmail.com>
2022-04-16 21:43:14 +0530

    commit 15

--- frontend/src/components/customers/addCustomers.js
+++ frontend/src/components/customers/addCustomers.js
@@ -2,8 +2,67 @@ import React, {useState} from "react";
 import Swal from "sweetalert2";
 import axios from "axios";
 
-export default function AddCustomer(){
+export default function AddItems(){
 
+    const[CustomerID,setCustomerID] = useState("");
+    const[Name,setName] = useState("");
+    const[NIC,setNIC] = useState("");
+    const[PhoneNumber,setPhoneNumber] = useState("");
+    const[Address,setAddress] = useState("");
+    const[Email,setEmail] = useState("");
+    const[Occupcation,setOccupcation] = useState("");
+    const[Gender,setGender] = useState("");
+    const[Member,setMember] = useState("");
+
+    let [errorMsg,setErrorMsg] = useState("");
+
+    function sendData(e){
+
+        e.preventDefault();
+
+        const newUser = {
+            CustomerID,
+            Name,
+            NIC,
+            PhoneNumber,
+            Address,
+            Email,
+            Occupcation,
+            Gender,
+            Member,
+            
+        }
+
+        console.log(newUser);
+  
+        axios.post("http://localhost:8070/user/add",newUser).then(()=>{
+  
+          setCustomerID(" ");
+          setName("");
+          setNIC(" ");
+          setPhoneNumber(" ");
+          setAddress(" ");
+          setEmail(" ");
+          setOccupcation(" ");
+          setGender(" ");
+          setMember(" ");
+
+          Swal.fire({
+            title: "Good job!",
+            text: "You send the messege!",
+            icon: "success",
+            button: "ok!"
+            
+        });
+
+    }).catch((err) =>{
+        alert(err)
+        
+        setErrorMsg(err.response.data.error);
+      })
+    }
+
+    
     return(
 
         <div className="customer">
@@ -16,26 +75,26 @@ export default function AddCustomer(){
                     <div className="col-sm-4">
                         <label for="Inputc-id" class="form-label" style={{color:'#3F3232', fontWeight:'bold'}}>Customer ID </label>
                         <input type="text" class="form-control" id="exampleInputtext1" aria-describedby="textHelp" style={{border:'1px solid #3F3232' }} 
-                       /* onChange={(e)=>{
-                            setTitle(e.target.value);
-                        }}*/
+                        onChange={(e)=>{
+                            setCustomerID(e.target.value);
+                        }}
                         required
                         />
                     </div>
                     <div className="col-sm-4">
                         <label for="Input-name" class="form-label" style={{color:'#3F3232', fontWeight:'bold'}}>Customer Name </label>
                         <input type="text" class="form-control" id="exampleInputtext1" aria-describedby="textHelp" style={{border:'1px solid #3F3232'}}
-                        // onChange={(e)=>{
-                        //     setAuthor(e.target.value);
-                        // }}
+                         onChange={(e)=>{
+                             setName(e.target.value);
+                         }}
                         required/>
                     </div>
                     <div className="col-sm-4">
                         <label for="Input-nic" class="form-label" style={{color:'#3F3232', fontWeight:'bold'}}>Customer NIC </label>
                         <input type="date" class="form-control" id="exampleInputtext1" aria-describedby="textHelp" style={{border:'1px solid #3F3232'}}
-                        // onChange={(e)=>{
-                        //     setDate(e.target.value);
-                        // }}
+                         onChange={(e)=>{
+                             setNIC(e.target.value);
+                         }}
                         required/>
                     </div>
                 </div>
@@ -44,25 +103,25 @@ export default function AddCustomer(){
                 <div class="col-sm-4">
                         <label for="Input-phone" class="form-label" style={{color:'#3F3232', fontWeight:'bold'}}>Phone Number <span style={{color:'red'}}>*</span> </label>
                         <input type="text" class="form-control" id="exampleInputtext1" aria-describedby="textHelp" style={{border:'1px solid #3F3232'}}
-                        // onChange={(e)=>{
-                        //     setPrice(e.target.value);
-                        // }}
+                         onChange={(e)=>{
+                             setPhoneNumber(e.target.value);
+                         }}
                         required/>
                     </div>
                     <div class="col-sm-4">
                         <label for="Input-address" class="form-label" style={{color:'#3F3232', fontWeight:'bold'}}>Address <span style={{color:'red'}}>*</span></label>
                         <input type="text" class="form-control" id="exampleInputtext1" aria-describedby="textHelp" style={{border:'1px solid #3F3232'}}
-                        // onChange={(e)=>{
-                        //     setQuantity(e.target.value);
-                        // }}
+                         onChange={(e)=>{
+                             setAddress(e.target.value);
+                         }}
                         required/>
                     </div>
                     <div class="col-sm-4">
                         <label for="Input-email" class="form-label" style={{color:'#3F3232', fontWeight:'bold'}}>Email<span style={{color:'red'}}>*</span> </label>
                         <input type="text" class="form-control" id="exampleInputtext1" aria-describedby="textHelp" style={{border:'1px solid #3F3232'}}
-                        // onChange={(e)=>{
-                        //     setCategory(e.target.value);
-                        // }}
+                         onChange={(e)=>{
+                             setEmail(e.target.value);
+                         }}
                         required/>
                     </div>
                 </div>
@@ -72,14 +131,15 @@ export default function AddCustomer(){
                 <div class="col-sm-4">
                         <label for="Input-occupcation" class="form-label" style={{color:'#3F3232', fontWeight:'bold'}}> Occupcation <span style={{color:'red'}}>*</span> </label>
                         <input type="text" class="form-control" id="exampleInputtext1" aria-describedby="textHelp" style={{border:'1px solid #3F3232'}}
-                        // onChange={(e)=>{
-                        //     setPrice(e.target.value);
-                        // }}
+                         onChange={(e)=>{
+                             setOccupcation(e.target.value);
+                         }}
                         required/>
                     </div>
                     <div class="col-sm-4">
                         <label for="Input-gender" class="form-label" style={{color:'#3F3232', fontWeight:'bold'}}> Gender<span style={{color:'red'}}>*</span></label>
                         <select name="Member" id="member">
+                            
                             <option value="Yes">Please select</option>
                             <option value="male">Male</option>
                             <option value="female">Female</option>
@@ -109,7 +169,7 @@ export default function AddCustomer(){
                         <div className="col-sm-3" style={{float:'right'}}>
                             <button type="submit" class="btn" style={{backgroundColor:'#3FC1C9',color:'#f5f5f5', fontWeight:'bold', width:'100px', float:'right', marginRight:'30px'}}>Submit</button>
                         </div>
-                    //yyuyuyuy
+                    
                     </div>
                 </div>
                 <div></div>
