ca03d3c2780ce7d01ecf8cf39798bb4ca44ec561
Vithusha T <vithutheva98@gmail.com>
2022-04-17 00:59:27 +0530

    commit 18

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -31,13 +31,11 @@ connection.once("open", () => {
 })
 
 //Make access to routes
-const itemRouter = require("./routes/items.js");
-app.use("/items", itemRouter);
+const customerRouter = require("./routes/customers.js");
+app.use("/customers", customerRouter);
 
 // const customerRouter = require("./routes/customers.js");
 // app.use("/customer", itemRouter);
-
-
 
 //Running the app on the defined port
 app.listen(PORT, () => {
@@ -47,15 +45,50 @@ app.listen(PORT, () => {
 
 
 
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+// const dotenv = require("dotenv");
+// const app = express();
+// const bodyParser = require("body-parser");
+// require("dotenv").config();
+
+// //Defining the port
+// const PORT = process.env.PORT || 8070;
+
+// // mount the specified middleware
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(express.json());
+
+// //Obtatain the Connection URL from .env file
+// const URL = process.env.MONGODB_URL;
+
+// //Building MongoDB Connection
+
+// mongoose.connect(URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
+
+// const connection = mongoose.connection;
+// connection.once("open", () => {
+//     console.log("MongoDB Connection Successful");
+// })
+
+// //Make access to routes
+// const itemRouter = require("./routes/items.js");
+// app.use("/items", itemRouter);
+
+// // const customerRouter = require("./routes/customers.js");
+// // app.use("/customer", itemRouter);
 
 
 
-
-
-
-
-
-
+// //Running the app on the defined port
+// app.listen(PORT, () => {
+//     console.log(`Server is up and running on PORT ${PORT}`);
+// });
 
 
 
