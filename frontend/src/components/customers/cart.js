7c5b4e1be35e21f5a87a5ad10d6bfe0403e25e70
TharinduDeshan <tharindudeshan50@gmail.com>
2022-04-30 21:32:48 +0530

    commit

--- frontend/src/components/customers/cart.js
+++ frontend/src/components/customers/cart.js
@@ -4,58 +4,122 @@ import { Link } from "react-router-dom";
 
 export default function Cart(props) {
 
-    let cart = [];
+    // let cart = [];
     let items = [];
     // let[items,setItems] = useState("");
-    let ItemIds = [];
+
+    let [ItemIds, setItemIds] = useState([]);
+    let [AllItems, setAllItems] = useState([]);
+
+    let [gItems, setgItems] = useState([]);
+    let AllItemsArr = [];
+
+    let [allItemsTotal, setAllItemsTotal] = useState(0);
+    // let [CartItems, setCartItems] = useState([]);
+
+    let ItemID = "";
+    let Title = "";
+    let Author = "";
+    let Category = "";
+    let Price = "";
+    let Quantity = "";
+    let SubTitle = "";
+    let Date = "";
+    let Image = "";
+  
+    let ItemDetails = {
+      ItemID,
+      Title,
+      Author,
+      Category,
+      Price,
+      Quantity,
+      SubTitle,
+      Date,
+      Image
+    };
 
     useEffect(() => {
         function getCart() {
           axios
             .get("http://localhost:8070/cart/getOneCart/625c12c9f36bd7f6a5c6748d")
             .then((res) => {
-                console.log(res.data)
-                cart = (res.data)
-                console.log(cart.itemIDs)
+                // console.log(res.data)
                
-                ItemIds = cart.itemIds
+                ItemIds = res.data.itemIDs
+                console.log(ItemIds)
 
-                for (let i = 0; i < cart.itemIDs.length; i++) {
-                    ItemIds = cart.itemIDs[i]
-                    // console.log(ItemIds)
-                  }
-                  console.log(ItemIds)
             })
             .catch((err) => {
               alert(err);
             });
+
+            axios
+            .get("http://localhost:8070/items/get")
+            .then((res) => {
+               
+
+                // const filter = res.data.filter(
+                //     (item) =>
+                //       item._id === ItemIds
+    
+                //   );
+                //   console.log(filter)
+                //   items=filter[0]
+                //   console.log(items)
+                //   console.log(items.Title)
+             
+                AllItems = res.data
+                console.log(AllItems)
+
+                getItemss(ItemIds,AllItems);
+
+            })
+            .catch((err) => {
+              alert(err.message);
+            });
         }
       
         getCart();
-        getItems();
       }, []);
 
-      function getItems(){
-        axios
-        .get("http://localhost:8070/items/get")
-        .then((res) => {
-            console.log(res.data)
-            const filter = res.data.filter(
-                (item) =>
-                  item._id === ItemIds
 
+      function getItemss(allItems, items) {
+        let j = 0;
+    
+        for (let i = 0; i < items.length; i++) {
+          j = 0;
+    
+          for (j = 0; j < allItems.length; j++) {
+            if (items[i] === allItems[j]._id) {
+              ItemDetails = {
+                ItemID: allItems[j]._id,
+                Title: allItems[j].Title,
+                Author: allItems[j].Author,
+                Category: allItems[j].Category,
+                Price: allItems[j].Price,
+                Quantity: allItems[j].Quantity,
+                SubTitle: allItems[j].SubTitle,
+                Image: allItems[j].Images[0],
+                Date: allItems[j].Date,
+              };
+    
+              console.log(allItemsTotal);
+              setAllItemsTotal(
+                Number(allItemsTotal) + Number(allItems[j].FinalPrice)
               );
-              console.log(filter)
-              items=filter[0]
-              console.log(items)
-              console.log(items.Title)
-         
-        })
-        .catch((err) => {
-          alert(err.message);
-        });
-        
+              allItemsTotal =
+                Number(allItemsTotal) + Number(allItems[j].FinalPrice);
+              AllItemsArr.push(ItemDetails);
+              
+            }
+          }
+        }
+    
+        setgItems(AllItemsArr);
       }
+        
+
 
 
   return (
@@ -69,8 +133,30 @@ export default function Cart(props) {
     <div className="row">
         <div className="col-7 ">
         <hr/>
-
-            <div className="row">
+        {gItems.map((item, index) => {
+                  return (
+                    <div className="row">
+                    <div className="col-3">
+                        <img style={{width:'200px'}}  src="../images/book1.jpg" />
+                    </div>
+                    <div className="col-6" style={{color:'#3F3232', fontWeight:'bold'}}>
+                        <span style={{fontSize:'18px'}}>{item.Title}</span>
+                        <br/>
+                        <span>&nbsp;&nbsp;{item.Author}</span>
+                        <br/>  <br/>
+                        <span>&nbsp;&nbsp;Rs.300/=</span>
+                    </div>
+                    <div className="col-3">
+                        
+                        <img style={{width:'200px'}}  src="../images/trash.png" />
+                        <br/><br/><br/>
+                        <label>QTY</label>
+                        <input type="number" pattern="[1-9]" Min="1" class="form-control" defaultValue='1' id="quantity" aria-describedby="textHelp" style={{border:'0px solid #3F3232', width:'70px'}}/>
+                       
+                    </div>
+                </div>
+                  )})}
+            {/* <div className="row">
                 <div className="col-3">
                     <img style={{width:'200px'}}  src="../images/book1.jpg" />
                 </div>
@@ -89,7 +175,7 @@ export default function Cart(props) {
                     <input type="number" pattern="[1-9]" Min="1" class="form-control" defaultValue='1' id="quantity" aria-describedby="textHelp" style={{border:'0px solid #3F3232', width:'70px'}}/>
                    
                 </div>
-            </div>
+            </div> */}
 
         </div>
 
