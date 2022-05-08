5ff5bc690f030707326744db50430486c45ec5a4
Vithusha T <vithutheva98@gmail.com>
2022-05-08 14:54:15 +0530

    commit

--- frontend/src/components/profile/userProfile.js
+++ frontend/src/components/profile/userProfile.js
@@ -1,8 +1,6 @@
 import React, { useState , useEffect} from "react";
 import axios from "axios";
-import Swal from "sweetalert2";
+
 import { Link, useNavigate, useParams } from "react-router-dom";
-import "../../css/search.css"
-import "datatables.net-dt/js/dataTables.dataTables"
-import "datatables.net-dt/css/jquery.dataTables.min.css"
+
 import $ from "jquery";
